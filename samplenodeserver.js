d9e6abbb7fb0dbf72bff4153e797abdd763a39dc
upathak1981 <95219280+upathak1981@users.noreply.github.com>
2024-03-20 22:26:45 +1100

    initialized directory

---
+++ samplenodeserver.js
@@ -0,0 +1,8 @@
+var conn = require("http");
+conn
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
